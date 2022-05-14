bcd3dfd4cd582b4c31009b21c67ee1dfc00237ef
Rajdeep Modi <modi.rajdeep@gmail.com>
2022-05-14 03:11:11 +0200

    installed nebular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import { NbThemeModule, NbAutocompleteModule, NbCardModule, NbSidebarModule, NbLayoutModule, NbButtonModule } from '@nebular/theme'
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 import { PlanListComponent } from './plan-list/plan-list.component';
 import { PlanFormComponent } from './plan-form/plan-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [NbThemeModule.forRoot(),
+    BrowserModule, FormsModule,ReactiveFormsModule, NbAutocompleteModule, NbCardModule, NbSidebarModule, NbLayoutModule, NbButtonModule, BrowserAnimationsModule, NbThemeModule.forRoot({ name: 'default' }), NbEvaIconsModule, AppRoutingModule],
   declarations: [
     AppComponent,
     HelloComponent,
--- src/app/plan-form/plan-form.component.ts
+++ src/app/plan-form/plan-form.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { FormControl } from '@angular/forms';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-plan-form',
   templateUrl: './plan-form.component.html',
-  styleUrls: ['./plan-form.component.css']
+  styleUrls: ['./plan-form.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlanFormComponent implements OnInit {
 
-  constructor() { }
+  options: string[];
+  filteredControlOptions$: Observable<string[]>;
+  filteredNgModelOptions$: Observable<string[]>;
+  inputFormControl: FormControl;
+  value: string;
 
   ngOnInit() {
+
+    this.options = ['Option 1', 'Option 2', 'Option 3'];
+    this.filteredControlOptions$ = of(this.options);
+    this.filteredNgModelOptions$ = of(this.options);
+
+    this.inputFormControl = new FormControl();
+    this.filteredControlOptions$ = this.inputFormControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(filterString => this.filter(filterString)),
+      );
+  }
+
+  private filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(optionValue => optionValue.toLowerCase().includes(filterValue));
+  }
+
+  onModelChange(value: string) {
+    this.filteredNgModelOptions$ = of(this.filter(value));
   }
 
 }
